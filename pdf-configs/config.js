0246cc4cb8576e31ee719955bd61f8091f295d95
yoshiyu0922 <yoshi.yoshiyu0922@gmail.com>
2026-03-20 21:47:12 +0900

    ci: remove headerTemplate and footerTemplate from pdf config

--- pdf-configs/config.js
+++ pdf-configs/config.js
@@ -9,8 +9,6 @@ module.exports = {
     "format": "A4",
     "margin": "30mm 20mm",
     "printBackground": true,
-    "headerTemplate": "<style>\n  section {\n    margin: 0 auto;\n    font-size: 9px;\n  }\n</style>",
-    "footerTemplate": "<section>\n  <div>\n    <span class=\"pageNumber\"></span>\n    / <span class=\"totalPages\"></span>\n  </div>\n</section>"
   },
   stylesheet_encoding: "utf-8",
   launch_options: {
